230cb448824bbc834ddc4a9f6f7113db0fe04ad2
anandkumar16 <anandyadav16022004@gmail.com>
2024-06-27 22:26:16 +0530

    first commit

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,12 @@
+export const Quote = () => {
+    return <div className="bg-state-200 flex h-screen flex justify-center flex-col ">
+        <div className="flex justify-center">
+            <div className="max-w-lg text-3xl font-bold">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, officia!
+            </div>
+        </div>
+        <div className="max-w-md text-center text-xl font-semibold">
+        warren buffet
+        </div>
+    </div>
+}
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://myapp.mrakgamers786.workers.dev";
